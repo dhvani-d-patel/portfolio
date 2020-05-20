927ee06d9db737d0674f3efea82bec0eceaba362
Dhvani Patel <dhvanypatel7@gmail.com>
2020-05-19 23:39:11 -0400

    Updates

---
+++ precache-manifest.727876bfe728961d7d29cd47ec7dd5f2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "160a451ca46e1d04ff13f828d8cdb649",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8fa1de3c4306abe708ee",
+    "url": "/portfolio/static/css/main.382cc86c.chunk.css"
+  },
+  {
+    "revision": "7e87bf3a33b3a39bdbca",
+    "url": "/portfolio/static/js/2.23222d61.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.23222d61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fa1de3c4306abe708ee",
+    "url": "/portfolio/static/js/main.3a5718f6.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "9bb3e9b3c1d9c32893e786d5914be5ff",
+    "url": "/portfolio/static/media/Dhvani_D_Patel_Resume.9bb3e9b3.pdf"
+  },
+  {
+    "revision": "b870e421f16a208cc2393e54c3b27a3e",
+    "url": "/portfolio/static/media/dp.b870e421.jpg"
+  },
+  {
+    "revision": "bbf4beea1f56427b74c3b8f22b7112f9",
+    "url": "/portfolio/static/media/galaxyImage.bbf4beea.jpg"
+  },
+  {
+    "revision": "71604d0d4173ea1828d36cfe4e23f41b",
+    "url": "/portfolio/static/media/logo.71604d0d.svg"
+  },
+  {
+    "revision": "686bd1da0d074aaadbf28668da139469",
+    "url": "/portfolio/static/media/oops.686bd1da.jpg"
+  }
+]);
